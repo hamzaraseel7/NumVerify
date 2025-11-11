d6fd886415c476b8e7f60e75b7aadef4e8565c23
hamzaraseel7 <hamzaraseel0718@gmail.com>
2025-11-11 22:29:56 +0530

    LiquidGlassUI

--- server/index.ts
+++ server/index.ts
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen({
     port,
-    host: "0.0.0.0", // Change 0.0.0.0 to localhost
+    host: "127.0.0.1", // Change 0.0.0.0 to localhost
     // Remove reusePort: true
   }, () => {
     log(`serving on port ${port}`);
